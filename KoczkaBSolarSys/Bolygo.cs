5a90110870b63aecbae0c5ac8a48ef3bb2dbbf58
Ny19KoczkaB <Ny19KoczkaB@203-06>
2023-10-24 11:57:16 +0200

    Add project files.

---
+++ KoczkaBSolarSys/Bolygo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoczkaBSolarSys
+{
+    internal class Bolygo
+    {
+        public string Nev { get; set; }
+        public int HoldSzam { get; set; }
+        public float TerfogatFoldhoz { get; set; }
+
+        public Bolygo(string p) 
+        {
+            var v = p.Split(';');
+            Nev = v[0];
+            HoldSzam= int.Parse(v[1]);
+            TerfogatFoldhoz = float.Parse(v[2]);
+        }
+    }
+}
---
+++ KoczkaBSolarSys/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+namespace KoczkaBSolarSys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bolygo> bolygok = new();
+            using StreamReader sr = new(@"..\..\..\src\solsys.txt", Encoding.UTF8);
+            while (!sr.EndOfStream) bolygok.Add(new Bolygo(sr.ReadLine()));
+            Console.WriteLine($"3.1: {bolygok.Count} bolygó van a naprendszerben");
+            var atlag = bolygok.Average(p=>p.HoldSzam);
+            Console.WriteLine($"3.2: a naprendszerbena bolygóknak átlagosan {atlag} holdjuk van");
+            var legnagyobb = bolygok.Max(p=>p.TerfogatFoldhoz);
+            Console.WriteLine($"3.3: a legnagyobb térfogatú bolygó a {legnagyobb}");
+            Console.Write("írd be a keresett bolygó nevét: ");
+            string kertbolygo = Console.ReadLine();
+            var keresett = bolygok.Where(p => p.Nev);
+            if ()
+
+        }
+        
+        
+    }
+}
